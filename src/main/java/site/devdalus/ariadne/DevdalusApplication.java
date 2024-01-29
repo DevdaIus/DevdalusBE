1c8cc83b5c59e493e4e1981f6de36b1eb24946a0
punkryn <gns8167@naver.com>
2024-01-29 11:13:22 +0900

    feat:프로젝트 초기화

---
+++ src/main/java/site/devdalus/ariadne/DevdalusApplication.java
@@ -0,0 +1,13 @@
+package site.devdalus.ariadne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevdalusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevdalusApplication.class, args);
+	}
+
+}
