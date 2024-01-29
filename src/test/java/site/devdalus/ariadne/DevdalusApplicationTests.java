4746ebf07f2e5bbd17e64ac7b3553ac4f94ef91d
punkryn <gns8167@naver.com>
2024-01-29 16:16:36 +0900

    feat:test ci 추가

--- src/test/java/site/devdalus/ariadne/DevdalusApplicationTests.java
+++ src/test/java/site/devdalus/ariadne/DevdalusApplicationTests.java
@@ -1,13 +1,14 @@
 package site.devdalus.ariadne;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DevdalusApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("test");
+		Assertions.assertThat(1).isEqualTo(0);
 	}
-
 }
