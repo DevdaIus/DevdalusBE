1a8008ecf52b2c9828e685b8c17b2895a593cb16
claycat <catenarywkd@naver.com>
2024-01-29 21:51:44 +0900

    fix: 테스트 제거

--- src/test/java/site/devdalus/ariadne/jpa/TestEntityRepository.java
+++
@@ -1,6 +0,0 @@
-package site.devdalus.ariadne.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
-}
