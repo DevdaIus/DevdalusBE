ff4d61129880a44812735f110dc328f6b09c7052
punkryn <gns816715@gmail.com>
2024-02-15 21:18:26 +0900

    feat: node service & test

--- src/main/java/site/devdalus/ariadne/repository/NodeRepository.java
+++ src/main/java/site/devdalus/ariadne/repository/NodeRepository.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Repository;
 import site.devdalus.ariadne.domain.Answer;
 import site.devdalus.ariadne.domain.Node;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface NodeRepository extends JpaRepository<Node, UUID> {
+    List<Node> findByParentId(UUID parentId);
 }
