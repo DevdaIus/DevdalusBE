e5ff07af0e29008d9ee814d1275b934e7a02752b
찰흙고양이 <claaycaat@gmail.com>
2024-03-03 19:57:51 +0900

    feat: add permission relationship

---
+++ src/main/java/site/devdalus/ariadne/domain/Permission.java
@@ -0,0 +1,31 @@
+package site.devdalus.ariadne.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import site.devdalus.ariadne.constant.PermissionName;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+public class Permission extends Base {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "permission_id")
+    private UUID permissionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Column(name = "permission_name", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PermissionName permissionName;
+
+}
